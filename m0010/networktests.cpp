7fff0736aa335a4ab4c65fdd24dfc2d499c9e29b
Wilk, Nicholas <nicholas.wilk@mines.sdsmt.edu>
2022-08-30 12:52:22 -0600

    m0010 a - [ci skip] - finished test cases for part A.

--- m0010/networktests.cpp
+++ m0010/networktests.cpp
@@ -15,6 +15,17 @@ TEST_CASE("compressOctets - testing classb ip adress")
 }
 
 
+TEST_CASE("compressOctets - testing another classb ip address")
+{
+    octet oct1 = 128;
+    octet oct2 = 90;
+    octet oct3 = 101;
+    octet oct4 = 45;
+    ip ipaddress = compressOctets(oct1, oct2, oct3, oct4);
+    REQUIRE(ipaddress == 2153407789);
+}
+
+
 TEST_CASE("compressOctets - testing classa ip address")
 {
     octet oct1 = 125;
@@ -25,3 +36,37 @@ TEST_CASE("compressOctets - testing classa ip address")
     REQUIRE(ipaddress == 2102267168);
 }
 
+
+TEST_CASE("compressOctets - testing classc ip address")
+{
+    octet oct1 = 202;
+    octet oct2 = 22;
+    octet oct3 = 101;
+    octet oct4 = 72;
+    ip ipaddress = compressOctets(oct1, oct2, oct3, oct4);
+    REQUIRE(ipaddress == 3390465352);
+}
+
+
+
+TEST_CASE("compressOctets - testing classD ip address")
+{
+    octet oct1 = 228;
+    octet oct2 = 119;
+    octet oct3 = 11;
+    octet oct4 = 35;
+    ip ipaddress = compressOctets(oct1, oct2, oct3, oct4);
+    REQUIRE(ipaddress == 3833006883);
+}
+
+
+TEST_CASE("compressOctets - testing classE ip address")
+{
+    octet oct1 = 251;
+    octet oct2 = 111;
+    octet oct3 = 66;
+    octet oct4 = 84;
+    ip ipaddress = compressOctets(oct1, oct2, oct3, oct4);
+    REQUIRE(ipaddress == 4218372692);
+
+}
