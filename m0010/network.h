dad135670e65e11489fd170d653d97bafd446f7e
Wilk, Nicholas <nicholas.wilk@mines.sdsmt.edu>
2022-08-31 13:05:19 -0600

    m0010 a -[ci test] - fixed part a hopefully.

--- m0010/network.h
+++ m0010/network.h
@@ -9,13 +9,9 @@ typedef unsigned int ip;
 
 ip compressOctets(octet oct1, octet oct2, octet oct3, octet oct4);
 
-void extractOctets(ip address, octet oct1, octet oct2, octet oct3, octet oct4);
+void extractOctets(ip address, octet &oct1, octet &oct2, octet &oct3, octet &oct4);
 
 networkType getNetworkType(ip address);
-
-
-
-
 
 
 
