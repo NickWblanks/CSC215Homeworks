0830ef6bd225425b07a6ffe1a043f467b5069ddb
Wilk, Nicholas <nicholas.wilk@mines.sdsmt.edu>
2022-09-08 14:54:29 -0600

    m0020 - [ci test] - fixed small error, now running vs grader.

--- m0020/m0020.cpp
+++ m0020/m0020.cpp
@@ -33,13 +33,13 @@ int main(int argc, char** argv)
     fin.open(argv[1]);
     if(!fin.is_open())
     {
-        cout << "Unable to open the input file: " << argv[1] << endl;
+        cout << "Unable to open input file: " << argv[1] << endl;
         return 0;
     }
     fout.open(argv[2]);
     if(!fout.is_open())
     {
-        cout << "Unable to open the output file: " << argv[2] << endl;
+        cout << "Unable to open output file: " << argv[2] << endl;
         return 0;
     }
     fin >> dataNum;
