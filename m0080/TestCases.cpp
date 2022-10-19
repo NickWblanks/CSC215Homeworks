e91fa30994a5ccfa6166ad9d4782fdb8ae9d055b
Wilk, Nicholas <nicholas.wilk@mines.sdsmt.edu>
2022-10-19 16:00:09 -0600

    m0080 a - [ci skip] - finished find and retrieveposition.

--- m0080/TestCases.cpp
+++ m0080/TestCases.cpp
@@ -28,3 +28,76 @@ TEST_CASE("Empty - Not an empty list")
     CHECK(list2.empty() == false);
     CHECK(list2.size() == 3);
 }
+
+TEST_CASE("Size - empty list")
+{
+    sortedSingle list;
+    CHECK(list.empty() == true);
+    CHECK(list.size() == 0);
+}
+
+TEST_CASE("Size - list with 1 item")
+{
+    sortedSingle list;
+    list.insert(1);
+    CHECK(list.empty() == false);
+    CHECK(list.size() == 1);
+}
+
+TEST_CASE("Find - empty list")
+{
+    sortedSingle list;
+    CHECK(list.find(10) == false);
+}
+
+TEST_CASE("Find - list without item")
+{
+    sortedSingle list;
+    list.insert(10);
+    list.insert(20);
+    list.insert(40);
+    CHECK(list.find(30) == false);
+}
+
+TEST_CASE("Find - list with item")
+{
+    sortedSingle list;
+    list.insert(10);
+    list.insert(20);
+    list.insert(30);
+    list.insert(40);
+    CHECK(list.find(30) == true);
+}
+
+TEST_CASE("retrievePosition - empty list")
+{
+    sortedSingle list;
+    CHECK(list.retrievePosition(10) == 0);
+}
+
+TEST_CASE("retrievePosition - list without item")
+{
+    sortedSingle list;
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    list.insert(5);
+    CHECK(list.retrievePosition(4) == 0);
+}
+
+TEST_CASE("retrievePosition - list with item")
+{
+    sortedSingle list;
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    list.insert(4);
+    CHECK(list.retrievePosition(4) == 4);
+}
+
+TEST_CASE("retrievePosition - list with only the item")
+{
+    sortedSingle list;
+    list.insert(99);
+    CHECK(list.retrievePosition(99) == 1);
+}
--- m0080/sortedSinglea.cpp
+++ m0080/sortedSinglea.cpp
@@ -15,6 +15,63 @@ bool sortedSingle::empty()
     return false;
 }
 
+int sortedSingle::size()
+{
+    node* temp;
+    temp = headptr;
+    int count = 0;
+    if (headptr == nullptr)
+    {
+        return count;
+    }
+    while (temp != nullptr)
+    {
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
+
+
+bool sortedSingle::find(int item)
+{
+    node* temp;
+    temp = headptr;
+    if (headptr == nullptr)
+    {
+        return false;
+    }
+    while (temp != nullptr)
+    {
+        if (temp->theItem == item)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+int sortedSingle::retrievePosition(int item)
+{
+    int count = 1;
+    node* temp;
+    temp = headptr;
+    if (headptr == nullptr)
+    {
+        return 0;
+    }
+    while (temp != nullptr)
+    {
+        if (temp->theItem == item)
+        {
+            return count;
+        }
+        count++;
+        temp = temp->next;
+    }
+    return 0;
+}
 sortedSingle::~sortedSingle()
 {
 
